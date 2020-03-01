b83a2e9d83f3ff4eaffd45e93855c8f706354bbd
Daniel Yde Therkildsen <36370310+StewyEarth@users.noreply.github.com>
2020-03-01 12:58:19 +0100

    Fixed misc task copying folders that it shouldn't

--- tasks/misc.js
+++ tasks/misc.js
@@ -2,13 +2,13 @@ const gulp = require("gulp");
 const connect = require("gulp-connect");
 
 function miscTask(){
-    return gulp.src("src/*")
+    return gulp.src("src/*.{xml,webmanifest,txt}")
     .pipe(gulp.dest('dist/'))
     .pipe(connect.reload())
 }
 
 function watchMisc(){
-    return gulp.watch("src/*",{ignoreInitial: false},miscTask)
+    return gulp.watch("src/*.{xml,webmanifest,txt}",{ignoreInitial: false},miscTask)
 }
 
 module.exports = {
